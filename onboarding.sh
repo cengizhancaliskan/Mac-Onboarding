a6d9dfc638bda93ef88f957388a5ea19ea874148
Cengizhan Çalışkan <cengizhancaliskan@users.noreply.github.com>
2021-06-13 21:24:41 +0300

    Update onboarding.sh

--- onboarding.sh
+++ onboarding.sh
@@ -1,7 +1,9 @@
-xcode-select --install \
+echo "Creating workspace folders" \\
+mkdir /Users/$USER/Workspace /Users/$USER/Workspace/ai /Users/$USER/Workspace/api /Users/$USER/Workspace/app /Users/$USER/Workspace/playground /Users/$USER/Workspace/projects /Users/$USER/Workspace/scripts /Users/$USER/Workspace/www \\
+&& xcode-select --install \
 
 && echo "Installing Homebrew" \\
-&& /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+&& /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo "Installing Bash v5" # mac os using version 3
 brew install bash
@@ -13,25 +15,15 @@ echo "Installing htop"
 brew install htop
 
 echo "Settings up zshrc configs"
-wget https://gist.githubusercontent.com/cengizhancaliskan/4801bb8c9c8cf0e974f2acd7da756459/raw/0eca94101947a18c349c62562cec6ee52313319e/.zshrc > ~/.zshrc
-
-echo "Installing GoLang"
-brew install go
-
-echo "Installing Java 1.8"
-brew cask install homebrew/cask-versions/adoptopenjdk8
-
-echo "Installinig .NET"
-brew cask install dotnet dotnet-sdk
-# For installing older version
-brew tap isen-ng/dotnet-sdk-versions
-brew cask install dotnet-sdk-2.2.100
+wget https://gist.githubusercontent.com/cengizhancaliskan/4801bb8c9c8cf0e974f2acd7da756459/raw/0eca94101947a18c349c62562cec6ee52313319e/.zshrc > ~/.zshrc.new
+mv .zshrc .zshrc.backup
+mv .zshrc.new .zshrc
 
 echo "Installing Git with homebrew"
 brew install git
 
 echo "Installing iTerm2"
-brew cask install iterm2
+brew install iterm2
 
 echo "Installing ZSH & Oh My ZSH"
 brew install zsh
@@ -47,16 +39,31 @@ echo "Installing Zsh Sytntax highlighting"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 echo "Installing Alfred"
-brew cask install alfred
+brew install alfred
 
 echo "Installing Itsycal"
-brew cask install itsycal
+brew install itsycal
 
 echo "Installing Spectacle"
-brew cask install spectacle
+brew install spectacle
+
+echo "Installing Stremio"
+brew install stremio
+
+echo "Installing GoLang"
+brew install go
+
+#echo "Installing Java 1.8"
+#brew cask install homebrew/cask-versions/adoptopenjdk8
+
+#echo "Installinig .NET"
+#brew cask install dotnet dotnet-sdk
+# For installing older version
+#brew tap isen-ng/dotnet-sdk-versions
+#brew cask install dotnet-sdk-2.2.100
 
 echo "Installing Docker Desktop"
-brew cask install docker
+brew install docker
 
 echo "Installing Minikube"
 brew install minikube
@@ -73,52 +80,52 @@ echo "Installing jEnv"
 brew install jenv
 
 echo "Installing SublimeText"
-brew cask install sublime-text
+brew install sublime-text
 
 echo "Installing F.lux"
-brew cask install flux
+brew install flux
 
-echo "Installing Chrome"
-brew cask install google-chrome
-
-echo "Installing Firefox"
-brew cask install firefox
-
-echo "Installing Evernote"
-brew cask install evernote
+echo "Installing Brave Browser"
+brew install brave-browser
 
 echo "Installing LastPass"
-brew cask install lastpass
+brew install lastpass
 
 echo "Installing Spotify"
-brew cask install spotify
+brew install spotify
 
 echo "Installing PyCharm"
-brew cask install pycharm
+brew install pycharm
+
+echo "Installing GoLand"
+brew install goland
+
+echo "Installing DataGrip"
+brew install datagrip
 
 echo "Installing CodeRunner"
-brew cask install coderunner
+brew install coderunner
 
 echo "Installing Ferdi (An alternative of Franz)"
-brew cask install ferdi
+brew install ferdi
 
-echo "Installing Studio 3T"
-brew cask install studio-3t
+#echo "Installing Studio 3T"
+#brew install studio-3t
 
 echo "Installing tldr"
 brew install tldr
 
 echo "Installing IINA"
-brew cask install iina
+brew install iina
 
 echo "Installing The Unarchiver"
-brew cask install the-unarchiver
+brew install the-unarchiver
 
-#echo "Installing VSCode"
-#brew cask install visual-studio-code
+echo "Installing VSCode"
+brew install visual-studio-code
 
-#echo "Installing Atom"
-#brew cask install atom
+#echo "Installing Macs Fan Control"
+#brew install macs-fan-control
 
 ## Create global gitignore
 echo "Creating a global gitignore.."
@@ -129,9 +136,9 @@ echo '.idea' >> ~/.gitignore
 echo "Global gitignore created"
 
 ## Settings git global config
-echo "Git global identity settings..."
-git config --global user.name "Cengizhan Çalışkan"
-git config --global user.email "cengizhancaliskan@gmail.com"
+#echo "Git global identity settings..."
+#git config --global user.name "Cengizhan Çalışkan"
+#git config --global user.email "cengizhancaliskan@gmail.com"
 
 # Brew cleanup
 brew cleanup -s
